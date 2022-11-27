e2103b42fa1f584d04e00f6e0fcba70f7c1817cc
LeonardoBerti07 <leonardo.berti07@gmail.com>
2022-11-27 16:26:16 +0100

    new

--- main.py
+++ main.py
@@ -5,42 +5,8 @@ from src.simulation.simulator import Simulator
 def main():
     """ the place where to run simulations and experiments. """
 
-    # a
-    # l
-    # eps
-    # div
-    # dist
-    data= np.load('src\Risultati.npy')
+    #FIRST PART GRID SEARCH: we search between the possible tuple of hyperparameters and we save the metrics for every one, with only one simulation for every tuple, n_drones
 
-    data = data[np.argsort(data[:, 5])]
-    data = np.flip(data, 0)
-    #print(data[:20])
-    sett = set()
-    for i in range(data.shape[0]-900):
-        drone, alpha, gamma, epsilon, div, metric = data[i]
-        sett.add((alpha, gamma, epsilon, div))
-
-    print(len(sett))
-
-
-    result = []
-    for tupla in sett:
-        somma = 0
-        for drone in range(5, 35, 5):
-            alpha, gamma, epsilon, div = tupla
-            sim = Simulator(drone, alpha, gamma, epsilon, div)
-            sim.run()
-            somma += len(sim.metrics.drones_packets_to_depot) / sim.metrics.all_data_packets_in_simulation
-            sim.close()
-        result.append((alpha, gamma, epsilon, div, somma))
-        print(len(result))
-
-    np.save("Risultati2.0", np.array(result))
-    result = np.array(result)
-    result = result[np.argsort(result[:, 4])]
-    result = np.flip(result, 0)
-    print(result[:20])
-'''
     alphas = [0.1, 0.2, 0.05, 0.01, 0.15]
     gammas = [0.1, 0.2, 0.05, 0.01, 0.5]
     epsilons = [15, 20, 25, 30]
@@ -59,13 +25,52 @@ def main():
 
     np.save("Risultati", np.array(results))
 
+    #SECOND PART GRID SEARCH: we choose the fifty tuples of parameters with the best metric and we try them with all the possible number of drones, toassure the generalization
+    data = np.load('src\Risultati.npy')
+    data = data[np.argsort(data[:, 5])]
+    data = np.flip(data, 0)
 
+    sett = set()
+    for i in range(data.shape[0] - 950):
+        drone, alpha, gamma, epsilon, div, metric = data[i]
+        sett.add((alpha, gamma, epsilon, div))
+
+    print(len(sett))
+
+    result = []
+    for tupla in sett:
+        somma = 0
+        for drone in range(5, 35, 5):
+            alpha, gamma, epsilon, div = tupla
+            sim = Simulator(drone, alpha, gamma, epsilon, div)
+            sim.run()
+            somma += len(sim.metrics.drones_packets_to_depot) / sim.metrics.all_data_packets_in_simulation
+            sim.close()
+        result.append((alpha, gamma, epsilon, div, somma))
+        #print(len(result))
+
+    np.save("Risultati2.0", np.array(result))
+
+
+    #THIRD PART GRID SEARCH: after testing the generalization of the best tuple of hyperparameters,
+    #we extract the best 10 tuples, so we can test them with the script for 30 seed for each num of drones, to assure the results stability.
+
+    result = np.array(result)
+    result = result[np.argsort(result[:, 4])]
+    result = np.flip(result, 0)
+    print(result[:10])
+
+
+
+
+
+
+'''
     sim = Simulator(5, 0.1, 0.1, 20, 50)  # empty constructor means that all the parameters of the simulation are taken from src.utilities.config.py
     sim.run()  # run the simulation
     print(len(sim.metrics.drones_packets_to_depot) / sim.metrics.all_data_packets_in_simulation)
     sim.close()
-
-    # for i in range(6):
 '''
+
 if __name__ == "__main__":
     main()
