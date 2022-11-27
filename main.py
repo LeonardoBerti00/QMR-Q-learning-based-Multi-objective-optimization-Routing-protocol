34c31acda558c5ab4ec4ad94bbbc052a42b2c9c7
LeonardoBerti07 <leonardo.berti07@gmail.com>
2022-11-27 16:31:43 +0100

    new

--- main.py
+++ main.py
@@ -61,20 +61,6 @@ def main():
     print(result[:10])
 
 
-
-    for drone in range(5, 30, 5):
-        for alpha in alphas:
-            for gamma in gammas:
-                for epsilon in epsilons:
-                    for div in divs:
-                        sim = Simulator(drone, alpha, gamma, epsilon, div)
-                        sim.run()
-                        results.append((drone, alpha, gamma, epsilon, div, len(sim.metrics.drones_packets_to_depot) / sim.metrics.all_data_packets_in_simulation))
-                        sim.close()
-
-    np.save("Risultati", np.array(results))
-
-
 '''
     sim = Simulator(5, 0.1, 0.1, 20, 50)  # empty constructor means that all the parameters of the simulation are taken from src.utilities.config.py
     sim.run()  # run the simulation
