08c5d768dc86bc91901d542850be4381859ea030
LeonardoBerti07 <98135135+LeonardoBerti07@users.noreply.github.com>
2022-12-02 12:44:34 +0100

    Update policies.py

--- src/utilities/policies.py
+++ src/utilities/policies.py
@@ -29,3 +29,13 @@ class UCB:
 
     def add(self, c):
         self.c = c
+        
+class egreedyGeo:
+    def __init__(self, epsilon=0):
+        self.epsilon = epsilon
+
+    def value(self):
+        return self.epsilon
+
+    def add(self, epsilon):
+        self.epsilon = epsilon
